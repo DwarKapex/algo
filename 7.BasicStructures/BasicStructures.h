e6a7823583dc47f28f638909a6fceff7a7a91513
Vladislav <vlad.kv.2002@gmail.com>
2020-05-18 20:15:57 -0700

    SparceArray remains. Need to clarify interface

---
+++ 7.BasicStructures/BasicStructures.h
@@ -0,0 +1,274 @@
+#pragma once
+
+#include <cstring>
+#include <vector>
+#include <list>
+
+namespace OtusAlgo {
+
+template <typename T>
+class IArray {
+public:
+    IArray() = default;
+    virtual ~IArray() = default;
+     
+    virtual void add(T item) = 0;
+    virtual size_t size() const noexcept = 0;
+    virtual void add(T item, int index) = 0;
+    virtual T get(int index) const noexcept = 0;
+    virtual T remove(int index) = 0;
+};
+
+
+template <typename T>
+class SingleArray: public IArray<T> {
+public:
+    SingleArray() = default;
+    ~SingleArray() {
+        delete [] m_array;
+    }
+
+    void add(T item) noexcept override {
+        resize();
+        m_array[m_size - 1] = item;
+    }
+    
+    size_t size() const noexcept override { return m_size; }
+
+    void add(T item, int index) override  {
+        add(item);
+        size_t idx = m_size-1;
+        while (idx > index) {
+            m_array[idx] = m_array[idx-1];
+            --idx;
+        }
+        m_array[index] = item;
+    }
+
+    T get (int index) const noexcept override {
+        if (out_of_boarder(index))
+            return {};
+        return m_array[index];
+    }
+    
+    T remove(int index) override {
+        if (out_of_boarder(index)) {
+            return {};
+        }
+        for (size_t i = index; i < m_size-1; ++i) {
+            m_array[i] = m_array[i+1];
+        }
+        --m_size;
+        return {};
+    }
+
+private:
+    void resize() {
+            T* new_array = new T[size() + 1];
+            std::memcpy(new_array, m_array, size());
+            delete [] m_array;
+            ++m_size;
+            m_array = new_array;
+    }
+    bool out_of_boarder(size_t index) const {
+        if (index >= size() || index < 0)
+            return true;
+        return false;
+    }
+    size_t m_size = 0;
+    T* m_array = nullptr;
+};
+
+
+template <typename T>
+class VectorArray: public IArray<T> {
+public:
+    VectorArray() = default;
+    VectorArray(int expand_step): m_expand_step(expand_step) {}
+
+    ~VectorArray() { delete [] m_array; }
+
+    void add(T item) noexcept override {
+        resize();
+        m_array[m_size - 1] = item;
+    }
+
+    size_t size() const noexcept override { return m_size; }
+
+    void add(T item, int index) override  {
+        add(item);
+        size_t idx = m_size-1;
+        while (idx > index) {
+            m_array[idx] = m_array[idx-1];
+            --idx;
+        }
+        m_array[index] = item;
+    }
+
+    T get (int index) const noexcept override {
+        if (out_of_boarder(index))
+            return {};
+        return m_array[index];
+    }
+    
+    T remove(int index) override {
+        if (out_of_boarder(index)) {
+            return {};
+        }
+        for (size_t i = index; i < m_size-1; ++i) {
+            m_array[i] = m_array[i+1];
+        }
+        --m_size;
+        return {};
+    }
+
+private:
+    bool out_of_boarder(size_t index) const {
+        if (index >= size() || index < 0)
+            return true;
+        return false;
+    }
+
+    void resize() {
+            T* new_array = new T[size() + m_expand_step];
+            std::memcpy(new_array, m_array, size());
+            delete [] m_array;
+            ++m_size;
+            m_array = new_array;
+    }
+    size_t m_expand_step = 10;
+    T* m_array = nullptr;
+    size_t m_size = 0;
+};
+
+
+template <typename T>
+class FactorArray: public IArray<T> {
+public:
+    FactorArray() = default;
+    ~FactorArray() { delete [] m_array; }
+
+    void add(T item) noexcept override {
+        resize();
+        m_array[m_size - 1] = item;
+    }
+
+    size_t size() const noexcept override { return m_size; }
+    
+    T get (int index) const noexcept override {
+        if (out_of_boarder(index))
+            return {};
+        return m_array[index];
+    }
+    
+    void add(T item, int index) override  {
+        add(item);
+        size_t idx = m_size-1;
+        while (idx > index) {
+            m_array[idx] = m_array[idx-1];
+            --idx;
+        }
+        m_array[index] = item;
+    }
+
+    T remove(int index) override {
+        if (out_of_boarder(index)) {
+            return {};
+        }
+        for (size_t i = index; i < m_size-1; ++i) {
+            m_array[i] = m_array[i+1];
+        }
+        --m_size;
+        return {};
+    }
+
+private:
+    bool out_of_boarder(size_t index) const {
+        if (index >= size() || index < 0)
+            return true;
+        return false;
+    }
+    void resize() {
+        ++m_size;
+        if (m_size >= m_capacity) {
+            m_capacity = m_capacity*m_expand_factor + 1;
+            T* new_array = new T[m_capacity];
+            std::memcpy(new_array, m_array, m_size-1);
+            delete [] m_array;
+            m_array = new_array;
+        }
+    }
+private:
+    T* m_array = nullptr;
+    size_t m_size = 0;
+    size_t m_capacity = 0;
+    size_t m_expand_factor = 2;
+};
+
+template <typename T>
+class VectorWrapper: public IArray<T> {
+public:
+    void add(T item) noexcept override {
+        m_array.push_back(item);
+    }
+
+    size_t size() const noexcept override { return m_array.size(); }
+    
+    T get (int index) const noexcept override {
+        return m_array.at(index);
+    }
+    
+    void add(T item, int index) override  {
+        m_array.insert(m_array.begin() + index, item);
+    }
+
+    T remove(int index) override {
+        m_array.erase(m_array.begin() + index);
+    }
+
+private:
+    std::vector<T> m_array;
+};
+
+template<typename T>
+class PrioretyQueue {
+    using Queue = std::vector<std::list<T>>;
+public:
+    void enqueue(int priority, T item) {
+        if (!is_priority_correct(priority)) return;
+        if (priority+1 > m_queue.size()) {
+            m_queue.resize(priority + 1);
+        }
+        m_queue[priority].push_back(item);
+    }
+    T dequeue() {
+        T result = m_queue.back().front();
+        m_queue.back().pop_front();
+        while (!m_queue.empty() && m_queue.back().size() == 0) {
+            m_queue.pop_back();
+        }
+        return result;
+    }
+private:
+    bool is_priority_correct(int priority) { return priority > 0; }
+    Queue m_queue;
+};
+
+
+template <typename T>
+class SparseMatrix: public IArray<T> {
+    using Type = std::list<T>;
+public:
+    void add(T item) override {
+        
+    }
+    size_t size() const noexcept;
+    void add(T item, int index);
+    T get(int index) const noexcept;
+    T remove(int index);
+
+private:
+    Type m_array;
+}
+} //namespace OtusAlgo
+
---
+++ 7.BasicStructures/main.cpp
@@ -0,0 +1,151 @@
+#include "BasicStructures.h"
+#include <fstream>
+#include <iostream>
+#include <filesystem>
+#include <chrono>
+#include <ctime>
+
+namespace fs = std::filesystem;
+
+template <typename T>
+void benchmark_add(std::ofstream& output, OtusAlgo::IArray<T>& array, std::string name) {
+    output << name << '\t';
+    constexpr size_t INSERT_ELEMENTS = 100000;
+    // singe_array
+    auto start = std::chrono::system_clock::now();
+    for (int i = 0; i < INSERT_ELEMENTS; ++i) {
+        array.add(i);
+    }
+    auto end = std::chrono::system_clock::now();
+    auto us = std::chrono::duration_cast<std::chrono::microseconds>(end-start).count();
+    output << us << " us\n";
+
+}
+
+template <typename T>
+void benchmark_insert_random(std::ofstream& output, OtusAlgo::IArray<T>& array, std::string name) {
+    output << name << '\t';
+    constexpr size_t INSERT_ELEMENTS = 10000;
+    std::vector<size_t> insert_pos;
+    for (int i = 0; i < INSERT_ELEMENTS; ++i) {
+        insert_pos.push_back((i*7 + 13) % 100000);
+    }
+    // singe_array
+    auto start = std::chrono::system_clock::now();
+    for (int i = 0; i < INSERT_ELEMENTS; ++i) {
+        array.add(i, insert_pos[i]);
+    }
+    auto end = std::chrono::system_clock::now();
+    auto us = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
+    output << us << " ms\n";
+}
+
+template <typename T>
+void benchmark_remove_random(std::ofstream& output, OtusAlgo::IArray<T>& array, std::string name) {
+    output << name << '\t';
+    constexpr size_t REMOVE_ELEMENTS = 10000;
+    std::vector<size_t> remove_pos;
+    for (int i = 0; i < REMOVE_ELEMENTS; ++i) {
+        remove_pos.push_back((i*13 + 7) % array.size() - REMOVE_ELEMENTS);
+    }
+    // singe_array
+    auto start = std::chrono::system_clock::now();
+    for (int i = 0; i < REMOVE_ELEMENTS; ++i) {
+        array.remove(remove_pos[i]);
+    }
+    auto end = std::chrono::system_clock::now();
+    auto us = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
+    output << us << " ms\n";
+
+}
+
+
+template <typename T>
+void benchmark_get_random(std::ofstream& output, OtusAlgo::IArray<T>& array, std::string name) {
+    output << name << '\t';
+    constexpr size_t INSERT_ELEMENTS = 10000;
+    std::vector<size_t> get_pos;
+    for (int i = 0; i < INSERT_ELEMENTS; ++i) {
+        get_pos.push_back((i*13 + 7) % array.size());
+    }
+    // singe_array
+    auto start = std::chrono::system_clock::now();
+    for (int i = 0; i < INSERT_ELEMENTS; ++i) {
+        array.get(get_pos[i]);
+    }
+    auto end = std::chrono::system_clock::now();
+    auto us = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
+    output << us << " ms\n";
+
+}
+
+
+int main() {
+    OtusAlgo::SingleArray<int> single_array;
+    OtusAlgo::VectorArray<int> vector_array;
+    OtusAlgo::FactorArray<int> factor_array;
+    OtusAlgo::VectorWrapper<int> vector_wrapper;
+    
+    std::filesystem::path this_file = __FILE__;
+    std::ofstream output(this_file.parent_path().string() + "/benchmark.txt");
+
+    // benchmark
+    // insert back
+//    output << "Adding back 1000000 elements:\n";
+    benchmark_add(output, single_array, "Single Array");
+    benchmark_add(output, vector_array, "Vector Array");
+    benchmark_add(output, factor_array, "Factor Array");
+    benchmark_add(output, vector_wrapper, "Vector Wrapper");
+
+
+    // insert random
+    output << "\nInserting at random position 10000 times:\n";
+    benchmark_insert_random(output, single_array, "Single Array");
+    benchmark_insert_random(output, vector_array, "Vector Array");
+    benchmark_insert_random(output, factor_array, "Factor Array");
+    benchmark_insert_random(output, vector_wrapper, "Vector Wrapper");
+
+    output << "\nRemoving at random position 10000 times:\n";
+    benchmark_remove_random(output, single_array, "Single Array");
+    benchmark_remove_random(output, vector_array, "Vector Array");
+    benchmark_remove_random(output, factor_array, "Factor Array");
+    benchmark_remove_random(output, vector_wrapper, "Vector Wrapper");
+
+    //
+    output << "\nGetting element at random position 10000 times:\n";
+    benchmark_get_random(output, single_array, "Single Array");
+    benchmark_get_random(output, vector_array, "Vector Array");
+    benchmark_get_random(output, factor_array, "Factor Array");
+    benchmark_get_random(output, vector_wrapper, "Vector Wrapper");
+
+
+    output << "\nSummary: using basic data structure such as array has both advantages and disadvantages."
+              "Advantages is obvious: element access time is O(1) because array is places as a continous "
+              "set of elements in RAM. Disadvantages: when the size of the array exceeds the capacity, we "
+              "need to reallocate more memory not only for a new elemnts, but for all elements in array, "
+              "copy all elements to a new place and only after that add new element. That takes time for"
+              " sure (on avarage O(n), where n is size of array). And less frequently we do that, more efficient "
+              "data structure we have. The same for removing and inserting elements in the middle: to keep data "
+              "structured continuesly in RAM, we have to copy all elements from inserting index. So on avarage "
+              "it is O(n) copying operations.";
+
+    
+    // PriorityQueue
+    OtusAlgo::PrioretyQueue<int> pq;
+    pq.enqueue(1, 10);
+    pq.enqueue(2, 20);
+    pq.enqueue(7, 30);
+    pq.enqueue(3, 40);
+    pq.enqueue(7, 50);
+    pq.enqueue(1, 60);
+    
+    std::string res = "";
+    for (int i = 0; i < 6; ++i) {
+        res += std::to_string(pq.dequeue()) + " ";
+    }
+        
+    std::string expected = "30 50 40 20 10 60 ";
+    std::cout << "PriorityQueue output: " << res << " ; is equal to expected: " << std::boolalpha << (res == expected) << std::endl;
+    
+    return 0;
+}
